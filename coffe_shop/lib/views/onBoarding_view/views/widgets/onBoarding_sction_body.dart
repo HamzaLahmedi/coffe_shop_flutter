beafb43e464d973a01927b1775d23663b55c2563
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-04-11 19:57:55 +0100

    Update sign in route to use lowercase for consistency
    Changed the route '/signIn' to '/signin' for consistency with other routes in the project.

--- coffe_shop/lib/views/onBoarding_view/views/widgets/onBoarding_sction_body.dart
+++ coffe_shop/lib/views/onBoarding_view/views/widgets/onBoarding_sction_body.dart
@@ -45,7 +45,7 @@ class OnBoardingSectionBody extends StatelessWidget {
                     title: 'Sign Up',
                     height: MediaQuery.sizeOf(context).height * 0.07,
                     width: MediaQuery.sizeOf(context).width * 0.9,
-                    onPressed: () => Navigator.pushNamed(context, '/signIn'),
+                    onPressed: () => Navigator.pushNamed(context, '/signin'),
                   ),
                 ],
               )
