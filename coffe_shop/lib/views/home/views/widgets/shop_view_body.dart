2db4ba49c716542efcd37a2720d6650156315d2a
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-04-29 12:31:21 +0100

    Update ShopViewBody with ShopFooter widget
    Add ShopFooter widget to display total, subtotal, shipping, and finalize order
    button in ShopViewBody. Use Provider to get cart price dynamically. and project ui done

--- coffe_shop/lib/views/home/views/widgets/shop_view_body.dart
+++ coffe_shop/lib/views/home/views/widgets/shop_view_body.dart
@@ -1,9 +1,11 @@
 import 'package:coffe_shop/core/utils/app_styles.dart';
 import 'package:coffe_shop/core/utils/colors.dart';
+import 'package:coffe_shop/provider/shopping_cart.dart';
 import 'package:coffe_shop/views/home/views/widgets/shop_list_view.dart';
 import 'package:coffe_shop/views/home/views/widgets/shop_view_header.dart';
 import 'package:coffe_shop/views/onBoarding_view/views/widgets/custom_transparant_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopViewBody extends StatelessWidget {
   const ShopViewBody({
@@ -26,86 +28,7 @@ class ShopViewBody extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            Container(
-              height: MediaQuery.sizeOf(context).height / 5,
-              width: MediaQuery.sizeOf(context).width / 0.5,
-              decoration: BoxDecoration(
-                color: AppColors.kPrimaryColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'Total',
-                    style: AppStyles.styleSemiBold24.copyWith(),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 6),
-                    child: Row(
-                      children: [
-                        Text(
-                          'subtotal ...........................................................................',
-                          style: AppStyles.styleRegular14.copyWith(
-                            color: AppColors.kDescriptionColor,
-                          ),
-                        ),
-                        Text(
-                          r'$20',
-                          style: AppStyles.styleSemiBold24.copyWith(
-                            fontSize: 13,
-                            color: AppColors.kBackgroundColor,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 6),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Shipping ...........................................................................',
-                          style: AppStyles.styleRegular14.copyWith(
-                            color: AppColors.kDescriptionColor,
-                          ),
-                        ),
-                        Text(
-                          r'$6',
-                          style: AppStyles.styleSemiBold24.copyWith(
-                            fontSize: 13,
-                            color: AppColors.kBackgroundColor,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 6),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Total ....................................................................................',
-                          style: AppStyles.styleRegular14.copyWith(
-                            color: AppColors.kDescriptionColor,
-                          ),
-                        ),
-                        Text(
-                          r'$26',
-                          style: AppStyles.styleSemiBold24.copyWith(
-                            fontSize: 13,
-                            color: AppColors.kBackgroundColor,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            ShopFooter(),
             const SizedBox(
               height: 15,
             ),
@@ -118,5 +41,97 @@ class ShopViewBody extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class ShopFooter extends StatelessWidget {
+  const ShopFooter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ShoppingCart>(builder: (context, cart, child) {
+      return Container(
+        height: MediaQuery.sizeOf(context).height / 5,
+        width: MediaQuery.sizeOf(context).width / 0.5,
+        decoration: BoxDecoration(
+          color: AppColors.kPrimaryColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Total',
+              style: AppStyles.styleSemiBold24.copyWith(),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: Row(
+                children: [
+                  Text(
+                    'subtotal ...........................................................................',
+                    style: AppStyles.styleRegular14.copyWith(
+                      color: AppColors.kDescriptionColor,
+                    ),
+                  ),
+                  Text(
+                    '\$ ${cart.price.toString()}',
+                    style: AppStyles.styleSemiBold24.copyWith(
+                      fontSize: 13,
+                      color: AppColors.kBackgroundColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: Row(
+                children: [
+                  Text(
+                    'Shipping ...........................................................................',
+                    style: AppStyles.styleRegular14.copyWith(
+                      color: AppColors.kDescriptionColor,
+                    ),
+                  ),
+                  Text(
+                    r'$6',
+                    style: AppStyles.styleSemiBold24.copyWith(
+                      fontSize: 13,
+                      color: AppColors.kBackgroundColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: Row(
+                children: [
+                  Text(
+                    'Total ....................................................................................',
+                    style: AppStyles.styleRegular14.copyWith(
+                      color: AppColors.kDescriptionColor,
+                    ),
+                  ),
+                  Text(
+                    cart.price > 0 ? '\$ ${cart.price + 6}' : '\$ 0',
+                    style: AppStyles.styleSemiBold24.copyWith(
+                      fontSize: 13,
+                      color: AppColors.kBackgroundColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
